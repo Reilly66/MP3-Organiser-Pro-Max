ab76098f7b7b74b6b9302c7ee2cf25577544427e
Reilly66 <reillyhowlett@icloud.com>
2022-05-11 22:01:01 +0100

    Created sign up page

--- website/auth.py
+++ website/auth.py
@@ -1,3 +1,7 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 auth = Blueprint("auth", __name__)
+
+@auth.route("/sign-up")
+def sign_up():
+    return render_template("sign_up.html")
