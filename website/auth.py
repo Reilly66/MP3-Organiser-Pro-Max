8945300f352c76ebb469a8c7629f3dfd7b719987
Reilly66 <reillyhowlett@icloud.com>
2022-05-12 20:05:55 +0100

    Finished login system

--- website/auth.py
+++ website/auth.py
@@ -2,6 +2,7 @@ from flask import Blueprint, render_template, request, flash, redirect, url_for
 from .models import User
 from .import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint("auth", __name__)
 
@@ -12,7 +13,9 @@ def sign_up():
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
 
-        if len(username) < 4:
+        if User.query.filter_by(username=username).first():
+            flash("Username is already taken.", "failure")
+        elif len(username) < 4:
             flash("Username must be at least 4 characters long.", category="failure")
         elif len(password1) < 8:
             flash("Password must be at least 8 characters long.", category="failure")
@@ -25,10 +28,32 @@ def sign_up():
             db.session.add(new_user)
             db.session.commit()
             flash("Account created.", category="success")
-            return redirect(url_for("views.home"))
+            login_user(new_user, remember=True)
+            return redirect(url_for("views.user_home"))
 
-    return render_template("sign_up.html")
+    return render_template("sign_up.html", user=current_user)
 
 @auth.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+
+        user = User.query.filter_by(username=username).first()
+        if user:
+            if check_password_hash(user.password, password):
+                flash("Logged in successfully.", category="success")
+                login_user(user, remember=True)
+                return redirect(url_for("views.user_home"))
+            else:
+                flash("Incorrect password.", category="failure")
+        else:
+            flash("Username does not exist.", category="failure")
+
+    return render_template("login.html", user=current_user)
+
+@auth.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for("views.home"))
--- website/views.py
+++ website/views.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template
+from flask_login import login_required, current_user
 
 views = Blueprint("views", __name__)
 
 @views.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", user=current_user)
+
+@views.route("/user-home")
+@login_required
+def user_home():
+    return render_template("user_home.html", user=current_user)
+
