971f6543b69a994682dd95d9aa48d340735a37b0
Reilly66 <reillyhowlett@icloud.com>
2022-05-12 17:47:45 +0100

    Added alerts

--- website/auth.py
+++ website/auth.py
@@ -1,7 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, flash
 
 auth = Blueprint("auth", __name__)
 
-@auth.route("/sign-up")
+@auth.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password1 = request.form.get("password1")
+        password2 = request.form.get("password2")
+
+        if len(username) < 4:
+            flash("Username must be at least 4 characters long.", category="failure")
+        elif len(password1) < 8:
+            flash("Password must be at least 8 characters long.", category="failure")
+        elif ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] in password1:
+            flash("Password must contain at least one number.", category="failure")
+        elif password1 != password2:
+            flash("Passwords must match.", category="failure")
+        else:
+            flash("Account created.", category="success")
+            # add user to the database
+
     return render_template("sign_up.html")
+
+@auth.route("/login", methods=["GET", "POST"])
+def login():
+    return render_template("login.html")
